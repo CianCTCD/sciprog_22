faf6845c62d3fcd7b6abc10bf9e078f303100f05
Cian Clarke <cianclarke@cians-mbp.home>
2022-09-30 18:50:57 +0100

    week1 just a test

---
+++ week1/c/hello_world.c
@@ -0,0 +1,26 @@
+#include <stdio.h>   
+
+int main(void) {
+
+
+printf("Hello World\n");
+
+
+return(0);
+
+}
+
+
+
+// Double slash = single line comments 
+
+/* slash + star gives you mult
+ * line coments*/
+
+
+// #include <stdio.h> ---> gives you access to all the i/o functionallity i.e. printf
+// #int main(void) ---> initalises the function like def in python, void means the function take no arguments 
+// return 0 is convention that the program finished succesfully 
+// every line must end with   ;
+// \n specifies new line 
+//
---
+++ week1/fortran/hello_world.f90
@@ -0,0 +1,14 @@
+! THIS IS THE MAIN PROGRAM
+
+PROGRAM helloworld
+
+WRITE(*,*) 'Hello World'
+
+END
+
+
+! the exclamation mark specifies a comment
+
+! the first astrik specifies to print to screen the second specifies to
+! use list directed io 
+! make sure to use straight quoates around print statements 
